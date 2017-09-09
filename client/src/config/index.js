75ce881e67746b181c0947f892d6ad395a372569
唐梦 <tangmeng@tangmengdeAir.lan>
2017-09-09 12:49:05 +0800

    新增纳新报名前端接口

---
+++ client/src/config/index.js
@@ -0,0 +1,14 @@
+export default {
+  api: {
+    admin: {
+      login: '/api/admin/login',
+      auth: '/api/admin/token'
+    },
+    signup: '/api/signup',
+    people: 'api/people'
+  },
+  cookie: {
+    secure: false,
+    httpOnly: false
+  },
+};
--- client/src/view/JoinUs/signUp.js
+++ client/src/view/JoinUs/signUp.js
@@ -1,60 +1,127 @@
 import React, { Component } from 'react';
-import { Input, Form, Button } from 'antd';
+import { Input, Form, Button,message } from 'antd';
+import 'whatwg-fetch';
+import xss from 'xss';
+
+import { signup } from '../../config';
 import styles from './index.css';
 
 const FormItem = Form.Item;
 const { TextArea } = Input;
+const options = {
+  whiteList: {
+    span: ['style'],
+    p: ['style'],
+    ins: ['style'],
+    strong: ['style'],
+    em: ['style'],
+    del: ['style'],
+    code: ['style'],
+    sup: ['style'],
+    sub: ['style'],
+    h1: ['style'],
+    h2: ['style'],
+    h3: ['style'],
+    h4: ['style'],
+    h5: ['style'],
+    h6: ['style'],
+    blockquote: ['style'],
+    ul: ['style'],
+    li: ['style'],
+    ol: ['style'],
+    a: ['style', 'href', 'target'],
+    iframe: ['width', 'height', 'style', 'src', 'frameborder'],
+    img: ['src', 'alt', 'style']
+  },
+  stripIgnoreTag: true,      // filter out all HTML not in the whilelist
+  stripIgnoreTagBody: ['script'] // the script tag is a special case, we need to filter out its content
+};
+const myxss = new xss.FilterXSS(options);
+
 
 class Index extends Component {
+  state={
+    loading: false,
+    data: []
+  }
+  handleSubmit=(e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, value) => {
+      value.stuIntro = myxss.process(value.stuIntro);
+      fetch(signup, {
+        method: 'POST',
+        header: { 'Content-type': 'application/json' },
+        body: JSON.stringify(...value)
+      }).then((res) => res.json()).then((res) => {
+        const { message, state } = res;
+        if (state) {
+          message.success(message);
+        } else {
+          message.error(message);
+        }
+      }).catch((error) => {
+        message.error(error);
+      });
+    });
+  }
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
       <div>
         <h3 className={styles.title}>纳新报名</h3>
-        <Form className={styles.form}>
+        <Form
+          className={styles.form}
+          onSubmit={this.handleSubmit}
+        >
           <FormItem >
-            {getFieldDecorator('StudentName', {
+            {getFieldDecorator('stuName', {
               rules: [{
                 required: true, message: '请输入姓名',
               }]
             })(<Input placeholder="请输入姓名" className={styles.input} />)}
           </FormItem>
           <FormItem >
-            {getFieldDecorator('studentSex', {
+            {getFieldDecorator('stuSex', {
               rules: [{
                 required: true, message: '请输入姓别',
               }]
             })(<Input placeholder="请输入姓别" className={styles.input} />)}
           </FormItem>
           <FormItem >
-            {getFieldDecorator('StudentClass', {
+            {getFieldDecorator('stuClass', {
               rules: [{
                 required: true, message: '请输入班级',
               }]
             })(<Input placeholder="请输入班级" className={styles.input} />)}
           </FormItem>
           <FormItem >
-            {getFieldDecorator('StudentId', {
+            {getFieldDecorator('stuNumber', {
               rules: [{
                 required: true, message: '请输入学号',
               }]
             })(<Input placeholder="请输入学号" className={styles.input} />)}
           </FormItem>
           <FormItem >
-            {getFieldDecorator('phoneNumber', {
+            {getFieldDecorator('stuTel', {
               rules: [{
                 required: true, message: '请输入电话号码',
               }]
-            })(<Input placeholder="联系方式" className={styles.input} />)}
+            })(<Input placeholder="联系电话" className={styles.input} />)}
           </FormItem>
           <FormItem >
-            {getFieldDecorator('volunteer', {
+            {getFieldDecorator('stuGroup', {
               rules: [{
                 required: true, message: '请选择',
               }]
             })(<Input placeholder="面试方向（安全组/开发组/待定)" className={styles.input} />)}
           </FormItem>
-          <TextArea rows={7} placeholder="自我介绍" className={styles.textarea} />
+          <FormItem>
+            {getFieldDecorator('stuIntro', {
+              rules: [{
+                required: true, message: '简单介绍一下自己呗~'
+              }]
+            })(<TextArea rows={7} placeholder="自我介绍" className={styles.textarea} />)}
+          </FormItem>
           <Button type="primary" htmlType="submit" className={styles.signUpBtn}>
              报名
           </Button>
