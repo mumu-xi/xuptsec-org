7d4b452b575a3433d0f35690367bb8ecf7a92383
唐梦 <tangmeng@tangmengdeAir.lan>
2017-09-13 22:04:06 +0800

    新增手机验证码登录

--- client/src/view/Admin/login.js
+++ client/src/view/Admin/login.js
@@ -5,7 +5,7 @@ import {
   Icon,
   Input,
   Button,
-  message
+  message,
 } from 'antd';
 import 'whatwg-fetch';
 import config from '../../config';
@@ -14,7 +14,29 @@ import styles from './index.css';
 const FormItem = Form.Item;
 const { login } = config.api.admin;
 class Index extends React.Component {
-
+  state = {
+    disabled: false,
+    inputVal: '点击获取手机验证码',
+    count: 60
+  };
+  handleVal = () => {
+    if (this.state.count === 0) {
+      this.setState({
+        count: 60,
+        inputVal: '点击获取手机验证码',
+        disabled: false
+      });
+      return;
+    }
+    this.setState({
+      count: this.state.count - 1,
+      inputVal: `重新发送(${this.state.count})s`,
+      disabled: true
+    });
+    setTimeout(() => {
+      this.handleVal();
+    }, 1000);
+  }
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.form.validateFields((err, value) => {
@@ -32,7 +54,7 @@ class Index extends React.Component {
           message.success('登录成功');
           browserHistory.push('/admin/students');
         } else {
-          console.log(2)
+          // console.log(2)
           message.error('请求失败');
         }
       })
@@ -67,6 +89,25 @@ class Index extends React.Component {
               validateTrigger: 'onBlur',
             })(
               <Input prefix={<Icon type="lock" style={{ fontSize: 13 }} />} type="password" placeholder="Password" />
+            )}
+          </FormItem>
+          <FormItem>
+            {this.props.form.getFieldDecorator('key', {
+              rules: [
+                { required: true, message: '请输入验证码' }
+              ],
+              validateTrigger: 'onBlur',
+            })(
+              <div style={{ display: 'flex' }}>
+                <Input type="text" placeholder="验证码" style={{ width: 100, marginRight: '20px' }} />
+                <Input
+                  type="button"
+                  disabled={this.state.disabled}
+                  value={this.state.inputVal}
+                  onClick={this.handleVal}
+                  style={{ width: 120 }}
+                />
+              </div>
             )}
           </FormItem>
           <Button type="primary" htmlType="submit" className={styles.loginBtn}>
