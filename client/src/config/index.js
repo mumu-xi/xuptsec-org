36b013f76148a4aff011e3a0dd1fc40b01d2018f
唐梦 <tangmeng@tangmengdeAir.lan>
2017-09-17 20:31:48 +0800

    新增spin

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -6,7 +6,6 @@ export default {
       auth: '/api/admin/token'
     },
     signup: 'http://mu.s1.natapp.cc /api/participator /insert',
-    // g4yriq.natappfree.cc/api/participator/find?pageNum=1&&pageSize=10 一次查十条报名人的信息
     getStudents: 'http://mu.s1.natapp.cc/api/manager/find',
     getPeople: 'http://mu.s1.natapp.cc/api/member/find'
   },
--- client/src/view/AboutUs/people.js
+++ client/src/view/AboutUs/people.js
@@ -1,49 +1,41 @@
 import React, { Component } from 'react';
 import 'whatwg-fetch';
-import { message } from 'antd';
+import { message, Spin } from 'antd';
 import styles from './index.css';
 import People from '../../components/People';
 import config from '../../config';
 
 const { getPeople } = config.api;
 
-// const data = [
-//   { name: '王琛 2014级 安全组负责人', intro: '免试保送进入浙江大学竺可桢学院求是科学班安徽省理科并列第四名考入清华计算机系，后保送清华高性能计算研究所，获硕士学位。多年深耕技术创业领域，在云计算、移动社交、多媒体处理等领域有着多年的技术积累。', picurl: '../../public/pic.jpg' },
-//   { name: '史欣鹭 2014级 开发组负责人', intro: '技术背景的连续创业者。安徽省理科并列第四名考入清华计算机系安徽省理科并列第四名考入清华计算机系，后保送清华高性能计算研究所，获硕士学位。多年深耕技术创业领域，在云计算、移动社交、多媒体处理等领域有着多年的技术积累。', picurl: '../../public/pic.jpg' }
-// ];
-// const data=[];
 export default class extends Component {
   state = {
     data: [],
-    // pagination: {},
     loading: false
   }
   componentDidMount() {
     this.getMenberInfo();
   }
   getMenberInfo = () => {
-    // console.log(1);
+    this.setState({ loading: true });
     fetch(getPeople, {
       method: 'GET'
     }).then((res) => res.json()).then((res) => {
-      // console.log(res);
       if (res.state === 'true') {
         this.setState({
-          data: res.data
+          data: res.data,
+          loading: false
         });
       } else {
         message.error(res.message);
       }
-      // console.log(this.state.data);
-      // this.props.data=data;
     });
   }
   render() {
     return (
-      <div>
+      <Spin spinning={this.state.loading}>
         <h3 className={styles.title}>人物简介</h3>
         <People data={this.state.data} />
-      </div>
+      </Spin>
     );
   }
 }
